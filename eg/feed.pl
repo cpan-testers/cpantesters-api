b03c76f898ea78fc10a9d351fa48feaae5765371
Doug Bell <doug@preaction.me>
2016-12-23 22:44:42 -0600

    make feed example auto-reconnect forever

--- eg/feed.pl
+++ eg/feed.pl
@@ -24,22 +24,28 @@ use experimental qw( signatures postderef );
 use Mojo::UserAgent;
 
 my $topic = $ARGV[0] || 'http://api.cpantesters.org/v1/upload';
-my $ua = Mojo::UserAgent->new(
-    inactivity_timeout => 60000,
-);
-$ua->websocket( $topic => sub( $ua, $tx ) {
-    say 'WebSocket handshake failed!' and return unless $tx->is_websocket;
-    say sprintf "[%s] Connected", scalar localtime;
-    $tx->on( json => sub( $tx, $upload ) {
-        say sprintf qq{[%s] Got upload: %s (%s) by %s},
-            scalar localtime, $upload->@{qw( dist version author )};
-    } );
-    $tx->on( finish => sub( $tx, $code ) {
-        say sprintf "[%s] Connection closed: %s",
-            scalar localtime, $code;
-        Mojo::IOLoop->stop;
-    } );
-} );
 
-Mojo::IOLoop->start unless Mojo::IOLoop->is_running;
+sub open_socket {
+    state $ua = Mojo::UserAgent->new(
+        inactivity_timeout => 60000,
+    );
+    $ua->websocket( $topic => sub( $ua, $tx ) {
+        say 'WebSocket handshake failed: ' . $tx->error->{message} and return unless $tx->is_websocket;
+        say sprintf "[%s] Connected", scalar localtime;
+        $tx->on( json => sub( $tx, $upload ) {
+            say sprintf qq{[%s] Got upload: %s (%s) by %s},
+                scalar localtime, $upload->@{qw( dist version author )};
+        } );
+        $tx->on( finish => sub( $tx, $code ) {
+            say sprintf "[%s] Connection closed: %s",
+                scalar localtime, $code;
+            Mojo::IOLoop->stop;
+        } );
+    } );
+}
+
+while ( 1 ) {
+    open_socket() && Mojo::IOLoop->start;
+    sleep 1;
+}
 

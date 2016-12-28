aa3a246074b5bbe0a982dc2f8d05965c6a6326ec
Doug Bell <doug@preaction.me>
2016-12-27 20:06:13 -0600

    incrementing version after release

--- lib/CPAN/Testers/API.pm
+++ lib/CPAN/Testers/API.pm
@@ -1,5 +1,5 @@
 package CPAN::Testers::API;
-our $VERSION = '0.006';
+our $VERSION = '0.007';
 # ABSTRACT: REST API for CPAN Testers data
 
 =head1 SYNOPSIS
--- lib/CPAN/Testers/API/Controller/Release.pm
+++ lib/CPAN/Testers/API/Controller/Release.pm
@@ -1,5 +1,5 @@
 package CPAN::Testers::API::Controller::Release;
-our $VERSION = '0.006';
+our $VERSION = '0.007';
 # ABSTRACT: API for test reports collected by CPAN release
 
 =head1 DESCRIPTION
